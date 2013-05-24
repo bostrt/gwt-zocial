f5d9651142a8f8db94c134fe81f20101c15ab771
Robert Bost <bostrt@gmail.com>
2013-05-23 22:15:18 -0400

    Added some more buttons. That's all for tonight.

---
+++ src/main/java/net/bostrt/gwt/zocial/client/button/DropboxButton.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2013 Robert Bost
+ */
+package net.bostrt.gwt.zocial.client.button;
+
+import net.bostrt.gwt.zocial.client.ZocialButton;
+import net.bostrt.gwt.zocial.client.resource.ZocialButtonResources;
+
+/**
+ *
+ * @author Robert Bost <bostrt at gmail dot com>
+ */
+public class DropboxButton extends ZocialButton
+{
+    public DropboxButton()
+    {
+        setStyleName(ZocialButtonResources.INSTANCE.zocial().zocial());
+        addStyleName(ZocialButtonResources.INSTANCE.zocial().dropbox());
+        setText("Sync with Dropbox");
+    }
+}
---
+++ src/main/java/net/bostrt/gwt/zocial/client/button/SoundcloudButton.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2013 Robert Bost
+ */
+package net.bostrt.gwt.zocial.client.button;
+
+import net.bostrt.gwt.zocial.client.ZocialButton;
+import net.bostrt.gwt.zocial.client.resource.ZocialButtonResources;
+
+/**
+ *
+ * @author Robert Bost <bostrt at gmail dot com>
+ */
+public class SoundcloudButton extends ZocialButton
+{
+    public SoundcloudButton()
+    {
+        setStyleName(ZocialButtonResources.INSTANCE.zocial().zocial());
+        addStyleName(ZocialButtonResources.INSTANCE.zocial().soundcloud());
+        setText("Follow me on Soundcloud");
+    }
+}
